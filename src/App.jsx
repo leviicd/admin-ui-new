f024a7d5f2ad64f474945d0cbbb57d66a0e5a194
leviicd <pahlevimuslim123@gmail.com>
2025-12-29 19:17:01 +0700

    State Management with Context

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import SignInPage from "./pages/signin";
+import SignUpPage from "./pages/signup";
+import ErrorPage from "./pages/Error";
+import DashboardPage from "./pages/dashboard";
+import BalancePage from "./pages/balance";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <DashboardPage />, 
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/login",
+      element: <SignInPage />,
+    },
+    {
+      path: "/register",
+      element: <SignUpPage />,
+    },
+    {
+      path: "/balance",
+      element: <BalancePage />,
+    }
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={myRouter} />
+    </>
+  );
+}
+
+export default App;
